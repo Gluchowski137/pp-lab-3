8618d0d4960befce11c285d71b4b29f9ab9dcbc0
PortingVR <krzysztofgluchowski2001@gmail.com>
2024-04-07 14:09:10 +0200

    zad5

--- Main.java
+++ Main.java
@@ -2,16 +2,8 @@ import geometry.*;
 
 public class Main{
     public static void main(String[] args) {
-        Circle[] circles = new Circle[]{
-                new Circle(new Point(0, 0), 1),
-                new ColoredCircle(new Point(1, 1), 2, "blue")
-        };
-
-        for (Circle circle : circles) {
-            System.out.println("Area: " + circle.getArea());
-            if (circle instanceof ColoredCircle) {
-                System.out.println("Color: " + ((ColoredCircle) circle).getColor());
-            }
-        }
+        Rectangle rectangle = new Rectangle(5.0, 3.0);
+        System.out.println("Rectangle area: " + rectangle.calculateArea());
+        System.out.println("Rectangle perimeter: " + rectangle.calculatePerimeter());
     }
 }
