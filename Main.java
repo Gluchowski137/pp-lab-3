18150fc3b3e55ea3dd5f0a5e46f10edadde4e262
PortingVR <krzysztofgluchowski2001@gmail.com>
2024-04-07 13:53:58 +0200

    zad1

---
+++ Main.java
@@ -0,0 +1,8 @@
+import geometry.*;
+
+public class Main{
+    public static void main(String[] args){
+        Point point = new Point(5.0, 10.0);
+        System.out.println("Point coordinates: (" + point.getX() + ", " + point.getY() + ")");
+    }
+}
