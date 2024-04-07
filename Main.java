f5a2c1869eeb1bc462b757620cb4eb3db6020f48
PortingVR <krzysztofgluchowski2001@gmail.com>
2024-04-07 13:58:10 +0200

    zad2

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ import geometry.*;
 
 public class Main{
     public static void main(String[] args){
-        Point point = new Point(5.0, 10.0);
-        System.out.println("Point coordinates: (" + point.getX() + ", " + point.getY() + ")");
+        Point center = new Point(0.0, 0.0);
+        Circle circle = new Circle(center, 5.0);
+        System.out.println("Circle center: " + circle.getCenter());
+        System.out.println("Circle radius: " + circle.getRadius());
+        System.out.println("Circle perimeter: " + circle.calculatePerimeter());
+        System.out.println("Circle area: " + circle.getArea());
     }
 }
